07ccccde37a2846a522871a38c5f87127a36fc0c
JUN <jun4404@gmail.com>
2020-02-26 01:14:31 +0900

    2020-02-26 파이썬

--- chapter03_01.py
+++ chapter03_01.py
@@ -19,8 +19,8 @@ str1 = "python"
 bool = True
 str2 = 'Anaconda'
 # 10 == 10.0 은 다르다. 정수타입, 실수타입
-float = 10.0
-int = 7
+float_v = 10.0
+int_v = 7
 list = [str1, str2]
 print(list)
 dict = {
@@ -34,8 +34,8 @@ set = {7, 8, 9, }
 print(type(str1))
 print(type(bool))
 print(type(str2))
-print(type(float))
-print(type(int))
+print(type(float_v))
+print(type(int_v))
 print(type(dict))
 print(type(tuple))
 print(type(set))
@@ -90,7 +90,6 @@ print("f1 + f2 : ", f1 + f2)
 print("bit_int1 + big_int2 : ", big_int1 + big_int2)
 print()
 
-
 # *
 print(">>>>>*")
 print("i1 * i2 : ", i1 * i2)
@@ -98,3 +97,33 @@ print("f1 * f2 : ", f1 * f2)
 print("bit_int1 * big_int2 : ", big_int1 * big_int2)
 print()
 
+# 형 변환 실습
+a = 3.
+b = 6
+c = .7
+d = 12.7
+
+# 타입 출력
+print(type(a), type(b), type(c), type(d))
+
+# 형 변환
+print(float(b))
+print(int(c))
+print(int(d))
+print(int(True))  # True : 1, False : 0
+print(float(False))
+print(complex(3))
+print(complex('3'))  # 문자형 -> 숫자형
+print(complex(False))
+
+# 수치 연산 함수
+print(abs(-7))
+x, y = divmod(100, 8)
+print(x, y)
+print(pow(5, 3), 5 ** 3)
+
+# 외부 모듈
+import math
+
+print(math.ceil(5.1)) # x이상의 수 중에서 가장 작은 정수
+print(math.pi)
